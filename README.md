fb55e3156eb981ef0d0fe73188808b25ae5876d2
squ1dd13 <squ1dd13dev@gmail.com>
2020-10-11 15:58:05 +0100

    Removed stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-# CSiOS
-Jailbreak tweak for injecting CLEO scripts into GTA: SA on iOS.
-
-[Video demo](https://www.youtube.com/watch?v=kA-q7CoM02E)
--- build.sh
+++
@@ -1,8 +0,0 @@
-# Hacky build script cos my Theos is fucked
-rm -r ./packages
-make clean package
-dpkg-deb -Z gzip -b .theos/_ ./package.deb
-tput bel
-scp ./package.deb root@192.168.1.226:/User/Downloads/package.deb
-tput bel
-ssh root@192.168.1.226 "dpkg -i /User/Downloads/package.deb && killall -9 gta3sa"
