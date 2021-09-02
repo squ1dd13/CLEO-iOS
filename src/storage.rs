6d2c14722965e90db877d89d1610362efbb88503
Squ1dd13 <squ1dd13dev@gmail.com>
2021-09-02 23:23:08 +0100

    Improve script logging

--- src/storage.rs
+++
@@ -1,8 +0,0 @@
-//! Handles storage of data kept between launches.
-
-use once_cell::sync::OnceCell;
-use std::collections::HashMap;
-
-static STORAGE: OnceCell<HashMap<String, serde_json::Value>> = OnceCell::new();
-
-pub fn init() {}
