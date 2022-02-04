36f4a7e81a4bcae6de69089d70da8726cdb90240
squ1dd13 <squ1dd13dev@gmail.com>
2022-02-04 17:32:59 +0000

    Use `pub use` instead of defining wrapper functions

--- src/scripts.rs
+++ src/scripts.rs
@@ -2,21 +2,7 @@ mod js;
 mod run;
 mod scm;
 
-pub fn load_invoked_script(path: &impl AsRef<std::path::Path>) -> eyre::Result<()> {
-    run::load_invoked_script(path)
-}
-
-pub fn load_running_script(path: &impl AsRef<std::path::Path>) -> eyre::Result<()> {
-    run::load_running_script(path)
-}
-
-pub fn tab_data_csa() -> crate::ui::TabData {
-    run::tab_data_csa()
-}
-
-pub fn tab_data_csi() -> crate::ui::TabData {
-    run::tab_data_csi()
-}
+pub use run::{load_invoked_script, load_running_script, tab_data_csa, tab_data_csi};
 
 pub fn init() {
     run::init();
